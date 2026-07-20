f27b249dbdb641404cd1a896577d72ae32b05fd7
acassol <a.cassol@bordeaux-metropole.fr>
2026-07-20 15:35:25 +0200

    add others distros

--- sidebar.js
+++ sidebar.js
@@ -6,16 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
   overlay.classList.add("overlay");
   document.body.appendChild(overlay);
 
+  sidebar.setAttribute("aria-hidden", "true");
+
   // Fonction pour ouvrir le menu
   function openMenu() {
       sidebar.classList.add("active");
       overlay.classList.add("active");
+      sidebar.setAttribute("aria-hidden", "false");
+      menuToggle.setAttribute("aria-expanded", "true");
   }
 
   // Fonction pour fermer le menu
   function closeSidebar() {
       sidebar.classList.remove("active");
       overlay.classList.remove("active");
+      sidebar.setAttribute("aria-hidden", "true");
+      menuToggle.setAttribute("aria-expanded", "false");
   }
 
   // Événements pour ouvrir et fermer la sidebar
